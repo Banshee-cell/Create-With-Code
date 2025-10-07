02f9491d5f77ec440c3b7df19692a80d0b1edbca
Banshee-cell <zacharyward.banshee@gmail.com>
2025-10-07 08:31:51 -0400

    Step into the drivers seat

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        //move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Moves the car forward on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on Horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
