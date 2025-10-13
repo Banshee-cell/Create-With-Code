80c06ed7b680fc266e0d7b1c4d779c3c4d523196
Banshee-cell <zacharyward.banshee@gmail.com>
2025-10-13 08:28:31 -0400

    10/13/25

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject projectilePrefab;
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
@@ -14,9 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    if (Input.GetKeyDown(KeyCode.Space))
     if (transform.position.x < -xRange) {
         transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
     }
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
