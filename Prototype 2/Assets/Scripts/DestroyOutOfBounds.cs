8b050e626b5f7d898399d7dace37b43db8f1e652
Banshee-cell <zacharyward.banshee@gmail.com>
2025-10-20 08:33:08 -0400

    Collision Decisions

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,8 +16,9 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
           Destroy(gameObject);
         } else if (transform.position.z < lowerBound) {
-          Destroy(gameObject);
-
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+            
         }}
 
         
