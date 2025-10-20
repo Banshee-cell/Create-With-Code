e4fea8757fdb74b4420f1cc56c484d86102a5558
Banshee-cell <zacharyward.banshee@gmail.com>
2025-10-20 08:00:07 -0400

    Random Animal Stampede

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -17,9 +17,10 @@ public class SpawnManager : MonoBehaviour
        if (Input.GetKeyDown(KeyCode.S)) {
             
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos -new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, new Vector3 (0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
         }
     }
 }
