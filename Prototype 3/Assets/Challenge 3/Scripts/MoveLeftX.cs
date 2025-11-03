92bc9359cefff266a2cb1d9cbd200297cb08b38d
Banshee-cell <zacharyward.banshee@gmail.com>
2025-11-03 08:28:56 -0500

    Balloons Bombs & Booleans

---
+++ Prototype 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed;
+    private PlayerControllerX playerControllerScript;
+    private float leftBound = -10;
+
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    void Update()
+    {
+        // Only move left if game is not over
+        if (!playerControllerScript.gameOver)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+
+        // Destroy objects that go off screen (but not the background)
+        if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
