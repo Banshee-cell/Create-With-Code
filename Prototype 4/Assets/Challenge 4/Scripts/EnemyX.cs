b629a8d93b2cedf02e32e31e79a28eb4a4602305
Banshee-cell <zacharyward.banshee@gmail.com>
2025-11-19 08:23:20 -0500

    Challenge 4 Soccer

---
+++ Prototype 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 5f; // you can adjust this
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        // Assign the player goal object (make sure it exists in the scene)
+        playerGoal = GameObject.Find("Player Goal");
+    }
+
+    void Update()
+    {
+        if (playerGoal != null)
+        {
+            // Set enemy direction towards player goal and move there
+            Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * speed);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal" || other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
