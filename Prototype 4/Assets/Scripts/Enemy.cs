ccad1509e878cfab3862d19fd8d09d8be0aae74a
Banshee-cell <zacharyward.banshee@gmail.com>
2025-11-19 08:01:10 -0500

    For Loops For Waves

--- Prototype 4/Assets/Scripts/Enemy.cs
+++ Prototype 4/Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody enemyRb;
     private GameObject player;
+  
 
     void Start()
     {
@@ -18,5 +19,11 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+
+
+        if (transform.position.y < -10) { Destroy(gameObject); }
     }
+  
+    
 }
--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,15 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber;
+    public GameObject powerupPrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
- 
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
         Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         
     }
@@ -17,8 +21,17 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    } 
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0) { waveNumber++; SpawnEnemyWave(waveNumber); Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
 
     private Vector3 GenerateSpawnPosition()
     {
